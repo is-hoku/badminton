ffa31080b7af73c8018f9a4bccaa0fbcf1e42834
nono-n1I <nono6217n@gmail.com>
2021-06-13 20:17:04 +0900

    fix get_frames and add shuttle

--- shuttle_point.cpp
+++ shuttle_point.cpp
@@ -1,32 +1,55 @@
 #include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
-#include <cvui.h>
 #include <string>
-#define CVUI_IMPLEMENTATION
+
+
+cv::Point p;
+
+void mouse_callback(int event, int x, int y, int flags, void *userdata){
+	bool *isClick = static_cast<bool *>(userdata);
+	if (event == cv::EVENT_LBUTTONDOWN){
+		*isClick = true;
+		p.x = x;
+		p.y = y;
+	}
+}
 
 
 int main(int argc, char** argv) {
-	if (argc != 2){
-		std::cout << "The number of arguments is incorrect." << std::endl;
-		return 1;
-	}
-	cv::VideoCapture cap;
-	cap.open(argv[1]);
-	if (!cap.isOpened()){
-		std::cout << "Cannot open " << argv[1] << "!" << std::endl;
-		return 1;
-	}
-	
-	for (int count = 2; count < cap.get(cv::CAP_PROP_FRAME_COUNT)-2; count++){
-		cv::Mat frame;
-		if (!cap.read(frame)){
-			std::cout << "Failed to extract a frame." << std::endl;
+	int key = 0;
+	bool flag = false;
+	bool isClick = false;
+	cv::namedWindow("Drawing point", cv::WINDOW_AUTOSIZE);
+	for (int i=0; i<10; i++){
+		if (flag == true){
+			if (i > 2){
+				i -= 2;
+			} else {
+				i = 0;
+			}
+			flag = false;
 		}
-		std::string path = "frames/" + std::to_string(count) + ".png";
-		cv::imwrite(path, frame);
+		cv::Mat img = cv::imread("./frames/"+std::to_string(i)+".png", cv::IMREAD_COLOR);
+		cv::setMouseCallback("Drawing point", mouse_callback, &isClick);
+		while (1){
+			if (isClick == true){
+				cv::circle(img, p, 3, cv::Scalar(0, 0, 255), -1);
+			}
+			cv::imshow("Drawing point", img);
+			key = cv::waitKey(1);
+			if (key == 'k'){
+				break;
+			}
+			else if (key == 'j'){
+				flag = true;
+				break;
+			}
+		}
+		isClick = false;
+		cv::imwrite(std::to_string(i)+".png", img);
 	}
-
-	cvui::init("drawing points");
 	
 	return 0;
 }
